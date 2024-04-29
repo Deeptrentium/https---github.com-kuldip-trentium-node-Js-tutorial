982f05cd92f1995b9d701a1d718847939d31fad9
kuldip-0611 <pandyakuldip2001@gmail.com>
2024-04-29 15:58:25 +0530

    work with json in node

--- index.js
+++ index.js
@@ -30,6 +30,10 @@
 //output ->kuldip , bhargav , bhargav
 
 
-const math = require('./export')
+// const math = require('./export')
 
-console.log(math.add(2,3))
+// console.log(math.add(2,3))
+
+const data = require('./data.json')
+//while importing json file add extendtion json bcz the node first check file of data.js then find the data.json 
+console.log(data.name)
