ed208ca8bf88382bd32d98fdd0b87d147dc2283e
kuldip-0611 <pandyakuldip2001@gmail.com>
2024-04-29 16:37:54 +0530

    path built in modules method

--- index.js
+++ index.js
@@ -34,6 +34,41 @@
 
 // console.log(math.add(2,3))
 
-const data = require('./data.json')
+// const data = require('./data.json')
 //while importing json file add extendtion json bcz the node first check file of data.js then find the data.json 
-console.log(data.name)
+// console.log(data.name)
+
+
+const path = require('path')
+
+console.log(__dirname)
+console.log(__filename)
+
+console.log(path.basename(__filename))
+
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
+
+console.log(path.format(path.parse(__filename)))
+
+console.log(path.isAbsolute(__filename)) //true
+
+console.log(path.isAbsolute('./data.json'))//false ./ is relative
+
+
+console.log(path.join('path1','path2',"index2"))
+
+console.log(path.join('/path1','path2',"index2"))
+
+console.log(path.join('/path1','///path2',"index2"))
+
+console.log(path.join('/path1','///path2',"../index2"))
+console.log(path.join(__dirname,"index.js"))
+
+
+console.log(path.resolve('folder1','folder2')) //->is not starts with / then return the dirnme 
+
+console.log(path.resolve('/folder1','folder2'))
+
+console.log(path.resolve('/folder1','//folder2')) //->resolve with consider with root 
